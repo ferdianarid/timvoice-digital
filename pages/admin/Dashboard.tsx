202444f270c0f48403406d455811ee0479e6e4e0
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-13 23:53:38 +0700

    update dashboard pages

---
+++ pages/admin/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+       return (
+              <div>
+                     <h1 className="text-5xl font-bold text-blue-800 p-[100px]">Dashboard Page</h1> 
+              </div>
+       )
+}
+
+export default Dashboard
---
+++ pages/admin/Invoices/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Invoices = () => {
+       return (
+              <div>
+                     <h1 className="text-5xl font-bold text-blue-800 p-[100px]">Invoices Page</h1> 
+              </div>
+       )
+}
+
+export default Invoices
