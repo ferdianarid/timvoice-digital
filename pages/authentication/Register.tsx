d5270646243ea128c8068a87aaf2a59ab94eb8cf
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-13 23:53:08 +0700

    add authentication

---
+++ pages/authentication/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+       return (
+              <div>
+                     <h1 className="text-5xl font-bold text-blue-800 p-[100px]">Register Page</h1> 
+              </div>
+       )
+}
+
+export default Register
