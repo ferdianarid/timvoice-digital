936cb43213f8b3c3d207483214d327b838aa7ff3
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-14 23:31:29 +0700

    tables and test local storage

---
+++ components/Tables.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+// interface Data Invoices
+interface InterfaceInvoices {
+       id: number,
+       created: string,
+       client: string,
+       project: string,
+       country: string,
+       amount: number,
+       status: string
+}
+
+// Data Invoices
+const DataInvoices = ( props: InterfaceInvoices ) => {
+       const  { id, created, client, project, country, amount, status } = props
+       return (
+              <tr className="">
+                     <th>
+                            <input type="checkbox" className="form-checkbox h-5 w-5 text-dark" name="" id="" />
+                     </th>
+                     <td className="px-4 py-3">{ id }</td>
+                     <td className="px-4 py-3">{ created }</td>
+                     <td className="px-4 py-3">{ client }</td>
+                     <td className="px-4 py-3">{ project }</td>
+                     <td className="px-4 py-3">{ country }</td>
+                     <td className="px-4 py-3">{ amount }</td>
+                     <td className="px-4 py-3">{ status }</td>
+                     <td className="px-4 py-3">000</td>
+              </tr>
+       )
+}
+
+const Tables = () => {
+       return (
+              <React.Fragment>
+                     <table className="text-blue-800 m-5 w-full mx-auto">
+                            <tr className="text-left border-b-2 border-indigo-300">
+                                   <th><input type="checkbox" className="form-checkbox h-5 w-5 text-dark" name="" id="" checked /></th>
+                                   <th className="px-4 py-3">ID</th>
+                                   <th className="px-4 py-3">Dibuat</th>
+                                   <th className="px-4 py-3">Klien</th>
+                                   <th className="px-4 py-3">Project</th>
+                                   <th className="px-4 py-3">Negara</th>
+                                   <th className="px-4 py-3">Total</th>
+                                   <th className="px-4 py-3">Status</th>
+                                   <th className="px-4 py-3">Dibayar</th>
+                            </tr>
+                            <DataInvoices id={335} created='27/12/2021' client='Ferdian Ahmad' project='Kubernetes' country='Singapore' amount={888} status='Dibayar' />
+                            <DataInvoices id={379} created='15/11/2021' client='John Smith' project='UI/UX Design' country='America' amount={742} status='Belum Dibayar' />
+                            <DataInvoices id={278} created='24/10/2021' client='John Doe' project='Copywriting' country='Malaysia' amount={456} status='Dibayar' />
+                            <DataInvoices id={335} created='27/12/2021' client='Ferdian Ahmad' project='Kubernetes' country='Singapore' amount={888} status='Dibayar' />
+                            <DataInvoices id={379} created='15/11/2021' client='John Smith' project='UI/UX Design' country='America' amount={742} status='Belum Dibayar' />
+                            <DataInvoices id={278} created='24/10/2021' client='John Doe' project='Copywriting' country='Malaysia' amount={456} status='Dibayar' />
+                            <DataInvoices id={335} created='27/12/2021' client='Ferdian Ahmad' project='Kubernetes' country='Singapore' amount={888} status='Dibayar' />
+                            <DataInvoices id={379} created='15/11/2021' client='John Smith' project='UI/UX Design' country='America' amount={742} status='Belum Dibayar' />
+                            <DataInvoices id={278} created='24/10/2021' client='John Doe' project='Copywriting' country='Malaysia' amount={456} status='Dibayar' />
+                     </table>
+              </React.Fragment>
+       )
+}
+
+export default Tables
---
+++ components/localStorage/index.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+
+// interface users
+interface InterfaceUsers {
+       id: number,
+       name: string,
+       email: string,
+       password: string
+}
+
+// form action Component
+const FormActions = ( ) => {
+       // set and change state
+       const [ name, setName ] = useState("")
+       const [ email, setEmail ] = useState("")
+
+       // use hook side effect
+       useEffect(() => {
+              // store state data to local storage
+              localStorage.setItem("name", JSON.stringify(name))
+              localStorage.setItem("email", JSON.stringify(email))
+       }, [name])
+
+       return (
+              <div>
+                     <React.Fragment>
+				<div className="w-full  h-auto flex flex-col md:flex-row justify-center md:justify-between">
+                                   <form className="max-w-sm m-auto my-6 rounded-lg border border-gray-300 py-8 px-10" action="/login" method="post">
+                                          <h1 className="text-2xl font-bold text-center pb-2">Local Storage</h1>
+                                          {/* <!-- Form Email Address --> */}
+                                          <div className="mb-4">
+                                                 <label htmlFor="email" className="block text-gray-700 text-sm font-medium mb-2">Email</label>
+                                                 <input type="text" onChange={(e) => setEmail(e.target.value)} id="email" name="email" value={email} placeholder="example@email.com" className="appereance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight" />
+                                          </div>
+                                          {/* <!-- Form Name --> */}
+                                          <div className="mb-4">
+                                                 <label htmlFor="name" className="block text-gray-700 text-sm font-medium mb-2">Name</label>
+                                                 <input type="name" onChange={(e) => setName(e.target.value)} id="name" value={name} name="name" placeholder="ferdian" className="appereance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight" />
+                                          </div>
+                                          {/* <!-- Input Checkbox --> */}
+                                          <div className="mb-4">
+                                                 <label htmlFor="checkbox" className="inline-flex items-center">
+                                                        <input type="checkbox" className="form-checkbox h-5 w-5 text-dark" name="" id="" checked />
+                                                        <span className="text-blue-800 pl-2">Remember me</span>
+                                                 </label>
+                                          </div>
+                                          <input type="submit" value="submit" className="mb-4 w-full text-lg text-center font-bold ml-0 no-underline inline-block px-4 py-3 leading-none bg-blue-800 border-blue-800 border rounded text-white hover:border-transparent hover:bg-white hover:text-blue-800 mt-4 sm:mt-0" />
+                                          {/* <!-- Forgot Password --> */}
+                                          <a href="/forgot-password" className="text-blue-800 text-sm">Forgot Password?</a>
+                                   </form>
+
+                                   <form className="max-w-sm m-auto my-6 rounded-lg border border-gray-300 py-8 px-10" action="/login" method="post">
+                                          <h1 className="text-2xl font-bold text-center pb-2">Local Storage</h1>
+                                          {/* <!-- Form Email Address --> */}
+                                          <div className="mb-4">
+                                                 <label htmlFor="email" className="block text-gray-700 text-sm font-medium mb-2">Email</label>
+                                                 <input type="text" onChange={(e) => setEmail(e.target.value)} id="email" name="email" value={email} placeholder="example@email.com" className="appereance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight" />
+                                          </div>
+                                          {/* <!-- Form Name --> */}
+                                          <div className="mb-4">
+                                                 <label htmlFor="name" className="block text-gray-700 text-sm font-medium mb-2">Name</label>
+                                                 <input type="name" onChange={(e) => setName(e.target.value)} id="name" value={name} name="name" placeholder="ferdian" className="appereance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight" />
+                                          </div>
+                                          {/* <!-- Input Checkbox --> */}
+                                          <div className="mb-4">
+                                                 <label htmlFor="checkbox" className="inline-flex items-center">
+                                                        <input type="checkbox" className="form-checkbox h-5 w-5 text-dark" name="" id="" checked />
+                                                        <span className="text-blue-800 pl-2">Remember me</span>
+                                                 </label>
+                                          </div>
+                                          <input type="submit" value="submit" className="mb-4 w-full text-lg text-center font-bold ml-0 no-underline inline-block px-4 py-3 leading-none bg-blue-800 border-blue-800 border rounded text-white hover:border-transparent hover:bg-white hover:text-blue-800 mt-4 sm:mt-0" />
+                                          {/* <!-- Forgot Password --> */}
+                                          <a href="/forgot-password" className="text-blue-800 text-sm">Forgot Password?</a>
+                                   </form>
+				</div>
+		       </React.Fragment>
+              </div>
+       )
+}
+
+export default FormActions
