0365ea36a526e70f4eb2ee445d9a0533adaa14f6
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-10 22:28:59 +0700

    config db mysql connection

---
+++ library/dbconnection.js
@@ -0,0 +1,11 @@
+const knex = require('knex')({
+    client: "mysql",
+    connection: {
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "timvoice"
+    }
+})
+
+export default knex
