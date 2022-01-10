c719d1f47e4dfbe912d547cbb25dd90be05f5843
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-11 01:25:28 +0700

    update app tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import isServer from "../utils/isServer"
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	return (
+		<SessionProvider session={session}>
+			< Component {...pageProps} />
+		</SessionProvider>
+	)
 }
 
 export default MyApp
