88cb27a09cd1704fe0daaf62efd6ef8cf5998915
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-12 00:30:55 +0700

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 import React from 'react'
-import FormActions from '../components/localStorage'
+import Image from "next/image"
 
 // import Homepage Layouts
 import HomepageLayouts from '../layouts/HomeLayouts'
+
+// import Hero Image
+import HeroImage from "../public/Hero.png"
 
 const Homepage: NextPage = () => {
 	return (
 		<React.Fragment>
 			<HomepageLayouts>
-				{/* <FormActions /> */}
+
 			</HomepageLayouts>
 		</React.Fragment>
 	)
