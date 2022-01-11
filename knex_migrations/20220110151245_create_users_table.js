7b6bc7c8326bb25df43e188d9bce24cb631dc6e9
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-11 20:16:31 +0700

    update knex migration

---
+++ knex_migrations/20220110151245_create_users_table.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('users', function(table) {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable().unique();
+        table.string('password').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+};
