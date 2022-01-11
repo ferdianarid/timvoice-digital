e2cdf81a1b677f2c0714b17e65cafb7383d8b6c5
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-12 00:30:23 +0700

    update homepage layouts

--- layouts/HomeLayouts.tsx
+++ layouts/HomeLayouts.tsx
@@ -1,15 +1,21 @@
 import React, { ReactNode } from 'react'
 
+// import Navbar Component
+import Navbar from "../components/Navbar"
+
 // interface Homepage Layout
 interface InterfaceHomepage {
        children: ReactNode
 }
 
 // Homepage Layouts
-const HomepageLayouts = ({ children } : InterfaceHomepage) => {
+const HomepageLayouts = ({ children }: InterfaceHomepage) => {
        return (
-              <div className="w-full main max-w-[1440px] m-auto px-0 pt-4 md:px-24 md:pt-8">
-                     { children }
+              <div className="background-gradient">
+                     <div className="w-full main max-w-[1440px] m-auto px-4 pt-4 md:px-24 md:pt-8">
+                            <Navbar />
+                            {children}
+                     </div>
               </div>
        )
 }
