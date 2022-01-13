8fbdc03639c49c9cc89ec937bc9b0d74e448865d
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-13 21:59:11 +0700

    update auth layouts

--- layouts/AuthLayouts.tsx
+++ layouts/AuthLayouts.tsx
@@ -1,16 +1,17 @@
 import React, { ReactNode } from 'react'
-
+import NavbarAuth from "../components/NavbarAuth"
 // interface Auth Layout
 interface AuthLayoutInterfaces {
        children: ReactNode
 }
 
 // AuthLayouts
-const AuthLayouts = ({ children } : AuthLayoutInterfaces) => {
+const AuthLayouts = ({ children }: AuthLayoutInterfaces) => {
        return (
               <React.Fragment>
-                     <div className="px-4 pt-4 max-w-[1440px] mx-auto md:px-24 md:pt-8">
-                            { children }
+                     <div className="max-w-[1440px] mx-auto px-4 md:px-24 md:pt-8">
+                            <NavbarAuth />
+                            {children}
                      </div>
               </React.Fragment>
        )
